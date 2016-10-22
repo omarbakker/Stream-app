4024f396b4eed5943c77dafd0a5b4a92c8b50760
Rohini Goyal <Rohini Goyal>
2016-10-21 19:09:59 -0700

    Initial dialogs for both reminder and review set up. Notifications appear for both

--- app/src/main/java/com/test/stream/stream/UI/sendReminderNotificationDialog.java
+++ app/src/main/java/com/test/stream/stream/UI/sendReminderNotificationDialog.java
@@ -1,12 +1,10 @@
 package com.test.stream.stream.UI;
 
-import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,48 +14,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RemoteViews;
-import android.widget.Switch;
 import android.widget.TextView;
+import com.test.stream.stream.UI.sendReviewNotificationDialog;
 
-import com.facebook.Profile;
 import com.test.stream.stream.R;
-
-import org.w3c.dom.Text;
 
 public class sendReminderNotificationDialog extends AppCompatActivity  implements View.OnClickListener{
 
-    Button getNotification;
+    Button getNotification, sendReview;
     final Context context = this;
+
+    //Reminder notification
     AlertDialog dialog;
     EditText messageToSend;
     TextView sendReminderAlertTitle;
     CheckBox sendAnonymously;
-    View v;
+    View reminderDialogView;
+
+    //Review notification
+    AlertDialog Reviewdialog;
+    EditText reviewMessageToSend;
+    TextView reviewTitle;
+    View reviewDialogView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_notification);
 
-        LayoutInflater inflater = LayoutInflater.from(context);
-        v = inflater.inflate(R.layout.send_reminder_notification, null);
-        messageToSend = (EditText) v.findViewById(R.id.reminderMessageToSend);
-
+        sendReview = (Button) findViewById(R.id.button2);
+        // Initializes views for reminder alert dialog
+        LayoutInflater ReminderInflater = LayoutInflater.from(context);
+        reminderDialogView = ReminderInflater.inflate(R.layout.send_reminder_notification, null);
+        messageToSend = (EditText) reminderDialogView.findViewById(R.id.reviewMessageToSend);
         getNotification = (Button) findViewById(R.id.getNotification);
         getNotification.setOnClickListener(this);
-
-        sendReminderAlertTitle = (TextView) findViewById(R.id.reminderTitle);
+        sendReview.setOnClickListener(this);
+        sendReminderAlertTitle = (TextView) findViewById(R.id.reviewTitle);
         sendAnonymously = (CheckBox) findViewById(R.id.sendAnonymously);
 
-
-
+        //Initialize AlertDialog for Reminder
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(v);
+        builder.setView(reminderDialogView);
         builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                String message = messageToSend.getText().toString();
-                getnotification(message);
+                String message = reviewMessageToSend.getText().toString();
+                getReminderNotification(message);
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -65,11 +67,37 @@ public class sendReminderNotificationDialog extends AppCompatActivity  implement
                 dialog.dismiss();
             }
         });
-       dialog = builder.create();
+        dialog = builder.create();
+
+        //
+        //------------------------------------------------------------------------------------
+        //
+
+        // Initializes views for review alert dialog
+        LayoutInflater ReviewInflater = LayoutInflater.from(context);
+        reviewDialogView = ReviewInflater.inflate(R.layout.send_review_notification, null);
+        reviewMessageToSend = (EditText) reviewDialogView.findViewById(R.id.reviewMessageToSend);
+        reviewTitle = (TextView) findViewById(R.id.reviewTitle);
+
+        //Initialize AlertDialog for Review
+        AlertDialog.Builder Reviewbuilder = new AlertDialog.Builder(this);
+        Reviewbuilder.setView(reviewDialogView);
+        Reviewbuilder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                String message = reviewMessageToSend.getText().toString();
+                getReviewNotification(message);
+            }
+        });
+        Reviewbuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        Reviewdialog = Reviewbuilder.create();
 
     }
 
-    public void getnotification(String message) {
+    public void getReminderNotification(String message) {
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
         nBuilder.setContentTitle("Here's a friendly reminder for you to complete your task!");
         nBuilder.setContentText(message);
@@ -77,6 +105,22 @@ public class sendReminderNotificationDialog extends AppCompatActivity  implement
         Notification notification = nBuilder.build();
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(1, notification);
+
+        //Clear contents of the EditText
+        messageToSend.setText("");
+    }
+
+    public void getReviewNotification(String message) {
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
+        nBuilder.setContentTitle("Here's a suggestion!");
+        nBuilder.setContentText(message);
+        nBuilder.setSmallIcon(R.drawable.com_facebook_button_icon);
+        Notification notification = nBuilder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+
+        //Clear contents of the EditText
+        reviewMessageToSend.setText("");
     }
 
     @Override
@@ -84,6 +128,9 @@ public class sendReminderNotificationDialog extends AppCompatActivity  implement
         switch (v.getId()){
             case R.id.getNotification:
                 dialog.show();
+                break;
+            case R.id.button2:
+                Reviewdialog.show();
                 break;
         }
     }
