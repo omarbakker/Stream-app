295a557cd4b42db0f67f8a4e3b3c262d55b318ea
omar bakker <omar_eyad@outlook.com>
2016-10-23 14:08:51 -0700

    Fixed bugs in projectsActivity, added newProjectActivity

---
+++ app/src/main/java/com/test/stream/stream/UI/newProjectActivity.java
@@ -0,0 +1,31 @@
+package com.test.stream.stream.UI;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.test.stream.streamtest.R;
+
+public class newProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
