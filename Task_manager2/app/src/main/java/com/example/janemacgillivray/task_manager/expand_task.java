6f7725f848c53abdff44c10c2bbcda04167e6cfa
Jane MacGillivray <jane..macgillivray@gmail.com>
2016-10-26 15:21:46 -0700

    Mostly working TaskUI

---
+++ Task_manager2/app/src/main/java/com/example/janemacgillivray/task_manager/expand_task.java
@@ -0,0 +1,78 @@
+package com.example.janemacgillivray.task_manager;
+
+/**
+ * Created by janemacgillivray on 2016-10-23.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+
+public class expand_task extends Activity {
+    ArrayList<Task> tasks = new ArrayList<>();
+    int current_task;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        //View parent = (View) view.getParent();
+        Log.d(TAG, "Enter expand_task");
+        super.onCreate(savedInstanceState);
+
+        Intent a = getIntent();
+        tasks = (ArrayList<Task>) a.getSerializableExtra("tasks");
+        String taskName = (String) a.getSerializableExtra("taskName");
+        View view = (View) a.getSerializableExtra("view");
+        setContentView(R.layout.item_details);
+
+        Log.d(TAG, "print statements for the win");
+        Task expandTask = new Task();
+        Log.d(TAG, "fuck everything");
+        int size = tasks.size();
+        Log.d(TAG, String.valueOf(size));
+        for (int i = 0; i < size; i++) {
+            //Log.d(TAG, "about to get it");
+            Task task = tasks.get(i);
+            //Log.d(TAG, "got it");
+            if (taskName.equals(task.getTASK_NAME())) {
+                Log.d(TAG, "task name comparisons");
+                Log.d(TAG, taskName);
+                Log.d(TAG, task.getTASK_NAME());
+                expandTask = task;
+                current_task = i;
+                break;
+            }
+        }
+        //Log.d(TAG, "pre-content view set");
+        setContentView(R.layout.item_details);
+
+
+        TextView task_name = (TextView) findViewById(R.id.task_name_expanded);
+        task_name.setText(expandTask.getTASK_NAME());
+
+        TextView task_description = (TextView) findViewById(R.id.description_expanded);
+        task_description.setText(expandTask.getTASK_DESCRIPTION());
+
+        TextView user = (TextView) findViewById(R.id.user_expanded);
+        user.setText(expandTask.getTASK_USER());
+
+        Log.d(TAG, "finished");
+
+    }
+
+    public void backToHome(){
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void markAsComplete(View view){
+        Task task = tasks.get(current_task);
+        task.setCOMPLETE(1);
+    }
+
+}
