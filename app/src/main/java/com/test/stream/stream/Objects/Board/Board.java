86ef21ccadc74644099a19807c86a56bcb3bc407
Kevin Wong <kevinwong_895@hotmail.com>
2016-10-26 19:14:03 -0700

    Linking firebase with Pinboard

--- app/src/main/java/com/test/stream/stream/Objects/Board/Board.java
+++ app/src/main/java/com/test/stream/stream/Objects/Board/Board.java
@@ -1,8 +1,66 @@
 package com.test.stream.stream.Objects.Board;
 
+import com.test.stream.stream.Utilities.PinType;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by cathe on 2016-10-01.
  */
 
 public class Board {
+    //region Variables
+    private String parentProjectId;
+    private Map<String, String> pins = new HashMap<String, String>();  //pin Id - pin type (message or file)
+    //endregion
+
+    //region Setters and Getters
+
+    public String getParentProjectId() {
+        return parentProjectId;
+    }
+    public Map<String, String> getPins() {
+        return pins;
+    }
+
+    //endregion
+
+    //region Constructors
+    public Board()
+    {
+        parentProjectId = "";
+    }
+    //endregion
+
+    //region Core Functions
+    public boolean addPin(String pinId, PinType type)
+    {
+        if(hasPin(pinId))
+        {
+            return false;
+        }
+
+        pins.put(pinId, type.toString());
+        return true;
+    }
+
+    public boolean removePin(String pinId)
+    {
+        if(!hasPin(pinId))
+        {
+            return false;
+        }
+
+        pins.remove(pinId);
+        return true;
+    }
+
+    public boolean hasPin(String pinId)
+    {
+        return pins.containsKey(pinId);
+    }
+
+    //endregion
+
 }
---
+++ app/src/main/java/com/test/stream/stream/Utilities/PinType.java
@@ -0,0 +1,10 @@
+package com.test.stream.stream.Utilities;
+
+/**
+ * Created by cathe on 2016-10-26.
+ */
+
+public enum PinType {
+    Message,
+    File
+}
