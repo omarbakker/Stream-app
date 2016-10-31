e0266c2f8fc6c26ae9b889e1084c2747700daadd
robyncastro <robyny3k@gmail.com>
2016-10-31 13:50:19 -0700

    Removed taskMessage and everything using it will now use tasks directly

--- app/src/main/java/com/test/stream/stream/UIFragments/ProjectHomeFragment.java
+++ app/src/main/java/com/test/stream/stream/UIFragments/ProjectHomeFragment.java
@@ -3,20 +3,18 @@ package com.test.stream.stream.UIFragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.github.lzyzsd.circleprogress.CircleProgress;
-import com.test.stream.stream.Controllers.ProjectManager;
 import com.test.stream.stream.Objects.Tasks.Task;
-import com.test.stream.stream.Objects.Tasks.TaskMessage;
 import com.test.stream.stream.R;
 import com.test.stream.stream.Utilities.TaskAdapter;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,7 +27,7 @@ public class ProjectHomeFragment extends Fragment {
 
     private static final String TAG = "Home Fragment";
 
-    ArrayList<TaskMessage> taskMessages = new ArrayList<>();
+    ArrayList<Task> taskMessages = new ArrayList<>();
     private TaskAdapter taskAdapter;
 
     public ProjectHomeFragment() {
@@ -63,6 +61,8 @@ public class ProjectHomeFragment extends Fragment {
         System.out.println("TeamProgress:" + teamProgress.getProgress());
 
         updateProgressBar(userProgress, 90);
+        updateUI();
+
     }
 
     private void updateProgressBar(CircleProgress progress, int newProgress){
@@ -74,23 +74,44 @@ public class ProjectHomeFragment extends Fragment {
     // Function that updates the Adapter of the ListFragment
     public void updateUI() {
         // Get all user tasks from the database
-        List<Task> allTasks = ProjectManager.sharedInstance().fetchCurrentUserProjects();
-        ArrayList<TaskMessage> tasks = new ArrayList();
+        List<Task> allTasks = new LinkedList<>();
+
+        System.out.println("UPDATING TASKS UI");
+        allTasks.add(createTask());
+        allTasks.add(createTask());
+        allTasks.add(createTask());
+
+        ArrayList<Task> tasks = new ArrayList();
         ListView listView = (ListView) getView().findViewById(R.id.task_list);
         // Go through each task in database
         for (Task currentTask : allTasks) {
-            if (currentTask.getClass() == TaskMessage.class) {
-                TaskMessage currentMessage = (TaskMessage) currentTask;
+            if (currentTask.getClass() == Task.class) {
+                System.out.println("Retrieving tasks");
+                Task currentMessage = (Task) currentTask;
                 tasks.add(currentMessage);
             }
         }
         if (taskAdapter == null) {
+            System.out.println("taskAdapter == null");
             taskAdapter = new TaskAdapter(getActivity(), taskMessages);
             listView.setAdapter(taskAdapter);
         } else {
+            System.out.println("taskAdapter != null");
             taskAdapter.clear();
             taskAdapter.addAll(tasks);
             taskAdapter.notifyDataSetChanged();
         }
+    }
+
+    private Task createTask(){
+        Task newTask = new Task();
+
+        newTask.setDueDay(10);
+        newTask.setDueMonth(10);
+        newTask.setDueYear(1990);
+
+        newTask.setDescription("Test description");
+        newTask.setName("Test name");
+        return newTask;
     }
 }
