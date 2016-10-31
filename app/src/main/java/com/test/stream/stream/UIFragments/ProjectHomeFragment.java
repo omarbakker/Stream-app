31aa973d401384c338198722af5565e0093e910a
robyncastro <robyny3k@gmail.com>
2016-10-31 12:49:55 -0700

    Added general structure for task adapter

--- app/src/main/java/com/test/stream/stream/UIFragments/ProjectHomeFragment.java
+++ app/src/main/java/com/test/stream/stream/UIFragments/ProjectHomeFragment.java
@@ -7,9 +7,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.github.lzyzsd.circleprogress.CircleProgress;
+import com.test.stream.stream.Controllers.ProjectManager;
+import com.test.stream.stream.Objects.Tasks.Task;
+import com.test.stream.stream.Objects.Tasks.TaskMessage;
 import com.test.stream.stream.R;
+import com.test.stream.stream.Utilities.TaskAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +29,9 @@ public class ProjectHomeFragment extends Fragment {
 
     private static final String TAG = "Home Fragment";
 
+    ArrayList<TaskMessage> taskMessages = new ArrayList<>();
+    private TaskAdapter taskAdapter;
+
     public ProjectHomeFragment() {
         // Required empty public constructor
     }
@@ -28,8 +39,11 @@ public class ProjectHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        System.out.println("OnCreateView");
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_project_home, container, false);
+        System.out.println("After view fetch");
+
         return view;
     }
 
@@ -37,11 +51,17 @@ public class ProjectHomeFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstance){
         super.onActivityCreated(savedInstance);
         View view = getView();
+        System.out.println("On Activity Created");
 
         teamProgress = (CircleProgress) view.findViewById(R.id.team_progress);
         userProgress = (CircleProgress) view.findViewById(R.id.user_progress);
 
+        System.out.println("TeamProgress:" + teamProgress.getProgress());
+
         updateProgressBar(teamProgress, 100);
+
+        System.out.println("TeamProgress:" + teamProgress.getProgress());
+
         updateProgressBar(userProgress, 90);
     }
 
@@ -51,4 +71,26 @@ public class ProjectHomeFragment extends Fragment {
         progress.setProgress(newProgress); // update progress
     }
 
+    // Function that updates the Adapter of the ListFragment
+    public void updateUI() {
+        // Get all user tasks from the database
+        List<Task> allTasks = ProjectManager.sharedInstance().fetchCurrentUserProjects();
+        ArrayList<TaskMessage> tasks = new ArrayList();
+        ListView listView = (ListView) getView().findViewById(R.id.task_list);
+        // Go through each task in database
+        for (Task currentTask : allTasks) {
+            if (currentTask.getClass() == TaskMessage.class) {
+                TaskMessage currentMessage = (TaskMessage) currentTask;
+                tasks.add(currentMessage);
+            }
+        }
+        if (taskAdapter == null) {
+            taskAdapter = new TaskAdapter(getActivity(), taskMessages);
+            listView.setAdapter(taskAdapter);
+        } else {
+            taskAdapter.clear();
+            taskAdapter.addAll(tasks);
+            taskAdapter.notifyDataSetChanged();
+        }
+    }
 }
