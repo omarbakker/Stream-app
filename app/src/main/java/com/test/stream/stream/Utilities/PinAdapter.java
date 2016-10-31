530611e2d4e5cfe5db5e7cc01d0e1ebe48f86c08
Kevin Wong <kevinwong_895@hotmail.com>
2016-10-30 23:19:14 -0700

    optimizing PinAdapter

--- app/src/main/java/com/test/stream/stream/Utilities/PinAdapter.java
+++ app/src/main/java/com/test/stream/stream/Utilities/PinAdapter.java
@@ -18,6 +18,11 @@ import java.util.ArrayList;
 
 public class PinAdapter extends ArrayAdapter<PinMessage> {
 
+    public static class ViewHolder{
+        TextView title;
+        TextView subTitle;
+        TextView description;
+    }
     public PinAdapter(Context context, ArrayList<PinMessage> pins){
         super(context, 0, pins);
     }
@@ -26,18 +31,24 @@ public class PinAdapter extends ArrayAdapter<PinMessage> {
     public View getView(int position, View convertView, ViewGroup parent){
 
         PinMessage pin = getItem(position);
+        ViewHolder viewHolder;
 
         if(convertView == null){
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_pin,parent, false);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.item_pin_title);
+            viewHolder.subTitle = (TextView) convertView.findViewById(R.id.item_pin_subtitle);
+            viewHolder.description = (TextView) convertView.findViewById(R.id.item_pin_description);
+            //Use set tag to remember viewHolder which is holding reference to widgets
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView pinTitle = (TextView) convertView.findViewById(R.id.item_pin_title);
-        TextView pinSubtitle = (TextView) convertView.findViewById(R.id.item_pin_subtitle);
-        TextView pinDescription = (TextView) convertView.findViewById(R.id.item_pin_description);
-
-        pinTitle.setText(pin.getTitle());
-        pinSubtitle.setText(pin.getSubtitle());
-        pinDescription.setText(pin.getDescription());
+        // Populate data into template view using data object
+        viewHolder.title.setText(pin.getTitle());
+        viewHolder.subTitle.setText(pin.getSubtitle());
+        viewHolder.description.setText(pin.getDescription());
 
         return convertView;
     }
