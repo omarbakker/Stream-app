947e2984f3e2d7f7ac3b9512a8b090defe2e9204
robyncastro <robyny3k@gmail.com>
2016-10-30 17:55:22 -0700

    Setting progress framework now works

--- app/src/main/java/com/test/stream/stream/UIFragments/ProjectHomeFragment.java
+++ app/src/main/java/com/test/stream/stream/UIFragments/ProjectHomeFragment.java
@@ -3,10 +3,12 @@ package com.test.stream.stream.UIFragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.lzyzsd.circleprogress.CircleProgress;
 import com.test.stream.stream.R;
 
 /**
@@ -14,17 +16,39 @@ import com.test.stream.stream.R;
  */
 public class ProjectHomeFragment extends Fragment {
 
+    private CircleProgress teamProgress;
+    private CircleProgress userProgress;
+
+    private static final String TAG = "Home Fragment";
 
     public ProjectHomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_project_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_project_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance){
+        super.onActivityCreated(savedInstance);
+        View view = getView();
+
+        teamProgress = (CircleProgress) view.findViewById(R.id.team_progress);
+        userProgress = (CircleProgress) view.findViewById(R.id.user_progress);
+
+        updateProgressBar(teamProgress, 100);
+        updateProgressBar(userProgress, 90);
+    }
+
+    private void updateProgressBar(CircleProgress progress, int newProgress){
+        progress.setProgress(0);           // initialize to 0
+        progress.setMax(100);              // set max to 100
+        progress.setProgress(newProgress); // update progress
     }
 
 }
