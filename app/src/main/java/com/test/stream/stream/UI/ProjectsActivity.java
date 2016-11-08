be95ef37427cef7af33782d5e7780ff1ee2147d3
rohinigoyal <rohinigoyal8@gmail.com>
2016-11-07 20:45:07 -0800

    Back button now exists application

--- app/src/main/java/com/test/stream/stream/UI/ProjectsActivity.java
+++ app/src/main/java/com/test/stream/stream/UI/ProjectsActivity.java
@@ -136,5 +136,13 @@ public class ProjectsActivity extends AppCompatActivity
 
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 
 }
