6394214ab608a5872af4db863dfc4aca178a4e69
robyncastro <robyny3k@gmail.com>
2016-11-07 16:00:06 -0800

    Added all the skeleton test files

---
+++ app/src/androidTest/java/com/test/stream/stream/LoginTest.java
@@ -0,0 +1,31 @@
+package com.test.stream.stream;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+/**
+ * Created by robyn on 2016-11-07.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+    @Test
+    public void FacebookLogin () throws Exception{
+
+    }
+
+    @Test
+    public void EmailLogin () throws Exception{
+
+    }
+
+    @Test
+    public void EmailSignup () throws Exception{
+
+    }
+}
