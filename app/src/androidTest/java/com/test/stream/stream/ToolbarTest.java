6f19b38fcc095871a08b1644ea1e56ddca19a8dd
robyncastro <robyny3k@gmail.com>
2016-11-07 16:14:36 -0800

    Added skeleton for ToolbarTest

--- app/src/androidTest/java/com/test/stream/stream/ToolbarTest.java
+++ app/src/androidTest/java/com/test/stream/stream/ToolbarTest.java
@@ -12,4 +12,49 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ToolbarTest {
+
+    @Test
+    public void fromListToHome() throws Exception{
+
+    }
+
+    @Test
+    public void toHome() throws Exception {
+
+    }
+
+    @Test
+    public void toCalendar() throws Exception {
+
+    }
+
+    @Test
+    public void toChat() throws Exception {
+
+    }
+
+    @Test
+    public void toBoard() throws Exception {
+
+    }
+
+    @Test
+    public void toTasks() throws Exception {
+
+    }
+
+    @Test
+    public void toSettings() throws Exception {
+
+    }
+
+    @Test
+    public void toProjects() throws Exception {
+
+    }
+
+    @Test
+    public void logOut() throws Exception {
+
+    }
 }
