45cfc3403b3ccd7ee5432c80c0c3b6d893cba82e
rohinigoyal <rohinigoyal8@gmail.com>
2016-11-07 18:48:59 -0800

    finished the JDoc comments for the user class

--- app/src/main/java/com/test/stream/stream/Objects/Users/User.java
+++ app/src/main/java/com/test/stream/stream/Objects/Users/User.java
@@ -22,41 +22,84 @@ public class User {
     //endregion
 
     //region Setters and getters
+
+    /**
+     * Sets the username of the user
+     * @param username
+     */
     public void SetUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the username of the user
+     * @return the username of the user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gets all of the projects that the user is part of
+     * @return Map of all of the projects mapping the Project name to a boolean indicating that the user is in the project
+     */
     public Map<String, Boolean> getProjects() {
         return projects;
     }
 
+    /**
+     * Gets all of teh tasks that the user is part of
+     * @return Map of all of the tasks that the user has been assigned mapped to the project
+     */
     public Map<String, String> getTasks() {
         return tasks;
     }
 
+    /**
+     * gets the User ID
+     * @return UserID
+     */
     public String getUid() {
         return uid;
     }
 
+    /**
+     * Sets the User ID
+     * @param uid
+     */
     public void setUid(String uid) {
         this.uid = uid;
     }
 
+    /**
+     * Gets the user's email
+     * @return user email
+     */
     public String getEmail() { return email; }
 
+    /**
+     * Sets the user's email
+     * @param email user email
+     */
     public void setEmail(String email) { this.email = email; }
 
+    /**
+     * Get's the name of the user
+     * @return name of the user
+     */
     public String getName() {return name;}
 
+    /**
+     * Sets the naem of the user
+     * @param name
+     */
     public void setName(String name) {this.name = name;}
 
     //endregion
 
-    //region Constructors
+    /**
+     * Constructor for the User
+     */
     public User()
     {
         this.uid = "";
@@ -66,6 +109,10 @@ public class User {
 
     }
 
+    /**
+     * Constructor for the user
+     * @param uid UID for the user
+     */
     public User(String uid) {
         this.uid = uid;
         this.username = " ";
@@ -73,6 +120,13 @@ public class User {
         this.name = "";
     }
 
+    /**
+     * Constructor for the user
+     * @param uid userID
+     * @param username user's username
+     * @param email user's email
+     * @param name user's name
+     */
     public User(String uid, String username, String email, String name) {
         this.username = username;
         this.uid = uid;
@@ -82,7 +136,12 @@ public class User {
 
     //endregion
 
-    //region Core Functions
+
+    /**
+     * Adds a new project to the user
+     * @param projectID
+     * @return flag indicating that the project was successfully added in
+     */
     public boolean addProject(String projectID) {
         if(isInProject(projectID))
         {
@@ -93,6 +152,11 @@ public class User {
         return true;
     }
 
+    /**
+     * Removes a project from the user
+     * @param projectId
+     * @return flag indicating that that the project was successfully removed
+     */
     public boolean removeProject(String projectId) {
         if(!isInProject(projectId))
         {
@@ -103,11 +167,22 @@ public class User {
         return true;
     }
 
+    /**
+     * Checks to see if the user is a member of a project
+     * @param projectId projectID of a proejct the user is being searched for
+     * @return flag indicating if the user is part of that project
+     */
     public boolean isInProject(String projectId)
     {
         return projects.containsKey(projectId);
     }
 
+    /**
+     * Adds a new task to the user
+     * @param taskId
+     * @param taskName
+     * @return flag indicating that the task was added in
+     */
     public boolean addTask(String taskId, String taskName) {
         if(hasTask(taskId))
         {
@@ -118,6 +193,11 @@ public class User {
         return true;
     }
 
+    /**
+     * Removes a task
+     * @param taskId
+     * @return flag indicating that the task was removed
+     */
     public boolean removeTask(String taskId){
         if(!hasTask(taskId))
         {
@@ -128,6 +208,11 @@ public class User {
         return true;
     }
 
+    /**
+     * Checks to see if a task has been assigned for the user
+     * @param taskId
+     * @return flag indicating that the task is assigned to the user
+     */
     public boolean hasTask(String taskId)
     {
         return tasks.containsKey(taskId);
