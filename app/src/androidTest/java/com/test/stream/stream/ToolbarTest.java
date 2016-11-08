b8adce8c87e4b92bbd6e1f52027349ed8478d48d
robyncastro <robyny3k@gmail.com>
2016-11-07 16:00:06 -0800

    Added all the skeleton test files

---
+++ app/src/androidTest/java/com/test/stream/stream/ToolbarTest.java
@@ -0,0 +1,15 @@
+package com.test.stream.stream;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+/**
+ * Created by robyn on 2016-11-07.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ToolbarTest {
+}
