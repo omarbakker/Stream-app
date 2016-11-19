73bbcf67dd1dd790eb200bc3cec57fe9f011f68f
Kevin Wong <kevinwong_895@hotmail.com>
2016-11-18 23:55:46 -0800

    Adding Teams UI

--- app/src/main/java/com/test/stream/stream/UIFragments/TeamFragment.java
+++ app/src/main/java/com/test/stream/stream/UIFragments/TeamFragment.java
@@ -7,11 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.test.stream.stream.Objects.Users.User;
+import com.test.stream.stream.Controllers.BoardManager;
 import com.test.stream.stream.Controllers.ProjectManager;
 import com.test.stream.stream.Controllers.TeamManager;
 import com.test.stream.stream.R;
 import com.test.stream.stream.Utilities.Listeners.DataEventListener;
+import com.test.stream.stream.Utilities.TeamAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +27,14 @@ import com.test.stream.stream.Utilities.Listeners.DataEventListener;
 public class TeamFragment extends Fragment {
     private TeamManager mTeamManager = new TeamManager();
 
+    ArrayList<User> users = new ArrayList();
+    private ListView mPinListView;
+    private TeamAdapter teamAdapter;
     private DataEventListener dataListener = new DataEventListener() {
         @Override
         public void onDataChanged() {
-            System.out.println("Num members " + mTeamManager.GetUsers().size());
-            //TODO: update the UI since the member list has changed.
+            //System.out.println("Num members " + mTeamManager.GetUsers().size());
+            updateUI();
         }
     };
 
@@ -31,10 +42,36 @@ public class TeamFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private void updateUI() {
+
+        List<User> allUsers = mTeamManager.GetUsers();
+        ArrayList<User> users = new ArrayList();
+        // Go through each user in database
+        for (User currentUser : allUsers) {
+            users.add(currentUser);
+        }
+        // Reverse User order to show newly created on top
+        Collections.reverse(users);
+        if (teamAdapter == null) {
+            // If nothing in adapter then create a new one and set the adapter to show users
+            teamAdapter = new TeamAdapter(getActivity(), this.users);
+            mPinListView.setAdapter(teamAdapter);
+            // Otherwise add all the users in the current adapter and notify that adapter changed
+        } else {
+            teamAdapter.clear();
+            teamAdapter.addAll(users);
+            teamAdapter.notifyDataSetChanged();
+        }
+
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        mPinListView = (ListView) getView().findViewById(R.id.list_team);
+        teamAdapter = new TeamAdapter(getActivity(), users);
+        mPinListView.setAdapter(teamAdapter);
         assert ProjectManager.sharedInstance().getCurrentProject() != null; //If we are in the project, the project should not be null.
         mTeamManager.Initialize(dataListener);
 
---
+++ app/src/main/java/com/test/stream/stream/Utilities/TeamAdapter.java
@@ -0,0 +1,80 @@
+package com.test.stream.stream.Utilities;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.test.stream.stream.Objects.Users.User;
+
+import java.util.ArrayList;
+import com.test.stream.stream.R;
+
+/**
+ * Created by kevinwong on 2016-11-18.
+ */
+
+public class TeamAdapter extends ArrayAdapter<User> {
+
+    private Context mContext;
+
+    public static class ViewHolder{
+        ImageView image;
+        TextView userName;
+        TextView userEmail;
+    }
+
+    public TeamAdapter(Context context, ArrayList<User> users){
+        super(context, 0, users);
+        mContext = context;
+    }
+
+    /**
+     * Logic for adding text to the title, subTitle and description fields of the PinMessage
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return the View
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        // Get the PinMessage that is being created
+        User user = getItem(position);
+        // Create the viewHolder that stores the Pins
+        ViewHolder viewHolder;
+        // if there is nothing on PinBoard, add a new viewHolder
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            Typeface ralewayBold = Typeface.createFromAsset(mContext.getAssets(), "Raleway-SemiBold.ttf");
+            Typeface raleway = Typeface.createFromAsset(mContext.getAssets(), "Raleway-Regular.ttf");
+            // Get the view for the pinboard
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_team,parent, false);
+            // Get the TextView elements on the PinBoard
+            viewHolder.image = (ImageView) convertView.findViewById(R.id.item_member_image);
+            viewHolder.userName = (TextView) convertView.findViewById(R.id.item_username);
+            viewHolder.userEmail = (TextView) convertView.findViewById(R.id.item_user_email);
+
+            viewHolder.userName.setTypeface(raleway);
+            viewHolder.userEmail.setTypeface(raleway);
+            //Use set tag to remember viewHolder which is holding reference to widgets
+            // Use the viewHolder
+            convertView.setTag(viewHolder);
+        } else {
+            // If convertView is not null, simply update
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate data into template view using data object
+        viewHolder.userName.setText(user.getName());
+        viewHolder.userEmail.setText(user.getEmail());
+
+        return convertView;
+    }
+}
+
+
