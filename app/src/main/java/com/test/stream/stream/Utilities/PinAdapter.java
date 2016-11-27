c298b30bcbb21896c2cef9bc65f703a745492a01
Kevin Wong <kevinwong_895@hotmail.com>
2016-11-27 14:03:40 -0800

    Pin sticky note colors

--- app/src/main/java/com/test/stream/stream/Utilities/PinAdapter.java
+++ app/src/main/java/com/test/stream/stream/Utilities/PinAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.graphics.Typeface;
+
 import com.test.stream.stream.Objects.Board.Pin;
 import com.test.stream.stream.R;
 
@@ -20,10 +21,11 @@ import java.util.ArrayList;
 public class PinAdapter extends ArrayAdapter<Pin> {
 
     private Context mContext;
+
     /**
      * Declare the elements of how the Pin should look
      */
-    public static class ViewHolder{
+    public static class ViewHolder {
         TextView title;
         TextView subTitle;
         TextView description;
@@ -31,35 +33,37 @@ public class PinAdapter extends ArrayAdapter<Pin> {
 
     /**
      * Constructor for making PinAdapter
+     *
      * @param context
      * @param pins
      */
-    public PinAdapter(Context context, ArrayList<Pin> pins){
+    public PinAdapter(Context context, ArrayList<Pin> pins) {
         super(context, 0, pins);
         mContext = context;
     }
 
     /**
      * Logic for adding text to the title, subTitle and description fields of the PinMessage
+     *
      * @param position
      * @param convertView
      * @param parent
      * @return the View
      */
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
+    public View getView(int position, View convertView, ViewGroup parent) {
 
         // Get the PinMessage that is being created
         Pin pin = getItem(position);
         // Create the viewHolder that stores the Pins
         ViewHolder viewHolder;
         // if there is nothing on PinBoard, add a new viewHolder
-        if(convertView == null){
+        if (convertView == null) {
             viewHolder = new ViewHolder();
             Typeface ralewayBold = Typeface.createFromAsset(mContext.getAssets(), "IndieFlower.ttf");
             Typeface raleway = Typeface.createFromAsset(mContext.getAssets(), "Raleway-Regular.ttf");
             // Get the view for the pinboard
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_pin,parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_pin, parent, false);
             // Get the TextView elements on the PinBoard
             viewHolder.title = (TextView) convertView.findViewById(R.id.item_pin_title);
             viewHolder.subTitle = (TextView) convertView.findViewById(R.id.item_pin_subtitle);
@@ -80,6 +84,14 @@ public class PinAdapter extends ArrayAdapter<Pin> {
         viewHolder.title.setText(pin.getTitle());
         viewHolder.subTitle.setText(pin.getSubtitle());
         viewHolder.description.setText(pin.getDescription());
+
+        if (position % 3 == 0) {
+            convertView.setBackgroundColor(Color.argb(255, 167, 222, 235));
+        } else if (position % 2 == 0) {
+            convertView.setBackgroundColor(Color.argb(255, 255, 255, 165));
+        } else {
+            convertView.setBackgroundColor(Color.argb(255, 244, 192, 203));
+        }
 
         return convertView;
     }
